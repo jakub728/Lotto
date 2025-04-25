bb0d22681006aef14f28afcc7a56c65a978896d6
Jakub <sulisz.jakub@gmail.com>
2025-04-25 17:20:13 +0200

    statistic

--- backend/server.js
+++ backend/server.js
@@ -36,21 +36,6 @@ app.get("/api/lottery-results", async (req, res) => {
 });
 
 app.get("/api/statistic", async (req, res) => {
-  const url = new URL(
-    "https://developers.lotto.pl/api/open/v1/lotteries/draw-statistics/numbers-frequency"
-  );
-
-  const params = {
-    index: 1,
-    size: 10,
-    sort: "frequency",
-    order: "DESC",
-  };
-
-  Object.keys(params).forEach((key) =>
-    url.searchParams.append(key, params[key])
-  );
-
   const options = {
     method: "GET",
     headers: {
@@ -59,18 +44,22 @@ app.get("/api/statistic", async (req, res) => {
     },
   };
 
+  const gameType = "EuroJackpot";
+  const url = new URL(
+    "https://developers.lotto.pl/api/open/v1/lotteries/draw-statistics/numbers-frequency"
+  );
+  url.searchParams.append("gameType", gameType);
+
   try {
     const response = await fetch(url, options);
     if (!response.ok) {
       throw new Error(`Could not fetch data: ${response.status}`);
     }
     const result = await response.json();
-    res.json(result);
+    res.json(result); // Przekazujemy dane z zewnętrznego API do klienta (frontend)
   } catch (error) {
     console.error("Fetch error:", error);
-    res
-      .status(500)
-      .json({ error: `Failed to fetch lottery data: ${error.message}` });
+    res.status(500).json({ error: "Failed to fetch lottery data" });
   }
 });
 
