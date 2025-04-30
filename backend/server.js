382b1f2d6e804ab890e5a4f80a282c84987f70fa
Jakub <sulisz.jakub@gmail.com>
2025-04-30 14:28:04 +0200

    done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,75 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
+const resultsPath = path.join(path.resolve(), "data", "lastResults.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/api/lottery-results", async (req, res) => {
+// app.get("/api/results", (req, res) => {
+//   fs.readFile(resultsPath, "utf8", (err, data) => {
+//     if (err) return res.status(500).json({ error: "Failed to read results" });
+//     res.json(JSON.parse(data));
+//   });
+// });
+
+// // POST new result
+// app.post("/api/results", (req, res) => {
+//   const newResult = req.body;
+
+//   fs.readFile(resultsPath, "utf8", (err, data) => {
+//     if (err) return res.status(500).json({ error: "Read error" });
+
+//     let results = JSON.parse(data);
+//     const exists = results.some((r) => r.dateId === newResult.dateId);
+
+//     if (exists) {
+//       return res.status(409).json({ message: "Result already exists" });
+//     }
+
+//     results.push(newResult);
+
+//     fs.writeFile(resultsPath, JSON.stringify(results, null, 2), (err) => {
+//       if (err) return res.status(500).json({ error: "Write error" });
+//       res.status(201).json({ message: "Result saved" });
+//     });
+//   });
+// });
+
+// app.get("/api/lottery-results", async (req, res) => {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//       secret: "FpOf7GpFJIXoDpIuMsuUnTztmYC3QHVG4dLi5FOF+wo=",
+//     },
+//   };
+
+//   const gameType = "EuroJackpot";
+//   const url = new URL(
+//     "https://developers.lotto.pl/api/open/v1/lotteries/draw-results/last-results/"
+//   );
+//   url.searchParams.append("gameType", gameType);
+
+//   try {
+//     const response = await fetch(url, options);
+//     if (!response.ok) {
+//       throw new Error(`Could not fetch data: ${response.status}`);
+//     }
+//     const result = await response.json();
+//     res.json(result); // Przekazujemy dane z zewnętrznego API do klienta (frontend)
+//   } catch (error) {
+//     console.error("Fetch error:", error);
+//     res.status(500).json({ error: "Failed to fetch lottery data" });
+//   }
+// });
+
+app.get("/api/results", async (req, res) => {
   const options = {
     method: "GET",
     headers: {
@@ -24,43 +86,44 @@ app.get("/api/lottery-results", async (req, res) => {
 
   try {
     const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`Could not fetch data: ${response.status}`);
-    }
+    if (!response.ok) throw new Error(`API error: ${response.status}`);
     const result = await response.json();
-    res.json(result); // Przekazujemy dane z zewnętrznego API do klienta (frontend)
-  } catch (error) {
-    console.error("Fetch error:", error);
-    res.status(500).json({ error: "Failed to fetch lottery data" });
-  }
-});
 
-app.get("/api/statistic", async (req, res) => {
-  const options = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      secret: "FpOf7GpFJIXoDpIuMsuUnTztmYC3QHVG4dLi5FOF+wo=",
-    },
-  };
+    const draw = result[1];
+    const id = draw.drawSystemId;
+    const newDate = `${draw.drawDate.slice(8, 10)}.${draw.drawDate.slice(
+      5,
+      7
+    )}.${draw.drawDate.slice(0, 4)}`;
+    const newFive = [...draw.results[0].resultsJson].sort((a, b) => a - b);
+    const newTwo = [...draw.results[0].specialResults].sort((a, b) => a - b);
 
-  const gameType = "EuroJackpot";
-  const totalDraws = 500;
-  const url = new URL(
-    "https://developers.lotto.pl/api/open/v1/lotteries/draw-statistics/numbers-frequency"
-  );
-  url.searchParams.append("gameType", gameType);
-  url.searchParams.append("totalDraws", totalDraws);
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`Could not fetch data: ${response.status}`);
-    }
-    const result = await response.json();
-    res.json(result); // Przekazujemy dane z zewnętrznego API do klienta (frontend)
+    // Czytaj plik
+    fs.readFile(resultsPath, "utf8", (err, data) => {
+      if (err) return res.status(500).json({ error: "Read error" });
+
+      let results = JSON.parse(data);
+      const exists = results.some((r) => r.dateId === id);
+
+      if (!exists) {
+        results.push({
+          date: newDate,
+          dateId: id,
+          five: newFive,
+          two: newTwo,
+        });
+
+        fs.writeFile(resultsPath, JSON.stringify(results, null, 2), (err) => {
+          if (err) return res.status(500).json({ error: "Write error" });
+          return res.json(results);
+        });
+      } else {
+        return res.json(results);
+      }
+    });
   } catch (error) {
-    console.error("Fetch error:", error);
-    res.status(500).json({ error: "Failed to fetch lottery data" });
+    console.error("Backend error:", error);
+    res.status(500).json({ error: "Failed to process results" });
   }
 });
 
