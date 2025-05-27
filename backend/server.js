b8c42f67b574b09d31cd493e5f71c32843a66f62
Jakub <sulisz.jakub@gmail.com>
2025-05-27 17:31:40 +0200

    done

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,25 @@ import fetch from "node-fetch";
 import cors from "cors";
 import fs from "fs/promises";
 import path from "path";
-import { existsSync, mkdirSync } from "fs";
+import dotenv from "dotenv";
+
 const resultsPath = path.join(path.resolve(), "data", "lastResults.json");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  {
-    username: "admin",
-    password: "1234",
-  },
-];
+let results = JSON.parse(await fs.readFile(resultsPath, "utf-8"));
 
 app.get("/api/results", async (req, res) => {
   const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
-      secret: "FpOf7GpFJIXoDpIuMsuUnTztmYC3QHVG4dLi5FOF+wo=",
+      secret: process.env.KEY,
     },
   };
 
@@ -48,17 +45,7 @@ app.get("/api/results", async (req, res) => {
     const newFive = [...draw.results[0].resultsJson].sort((a, b) => a - b);
     const newTwo = [...draw.results[0].specialResults].sort((a, b) => a - b);
 
-    let results = [];
-    try {
-      const fileContent = await fs.readFile(resultsPath, "utf-8");
-      results = JSON.parse(fileContent);
-    } catch (e) {
-      console.log(e);
-    }
-
-    const exists = results.some((r) => r.dateId === id);
-
-    if (!exists) {
+    if (!results.some((r) => r.dateId === id)) {
       results.push({
         date: newDate,
         dateId: id,
@@ -66,9 +53,13 @@ app.get("/api/results", async (req, res) => {
         two: newTwo,
       });
 
-      await fs.writeFile(resultsPath, JSON.stringify(results, null, 2), {
-        encoding: "utf-8",
-      });
+      await fs.writeFile(
+        resultsPath,
+        JSON.stringify(results, null, 2),
+        "utf-8"
+      );
+    } else {
+      console.log(`Wynik numer ${id} już zapisany`);
     }
 
     res.json(results);
@@ -77,6 +68,13 @@ app.get("/api/results", async (req, res) => {
     res.status(500).json({ error: "Failed to process results" });
   }
 });
+
+const users = [
+  {
+    username: "admin",
+    password: "1234",
+  },
+];
 
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
