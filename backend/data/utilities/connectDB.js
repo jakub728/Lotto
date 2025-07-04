967a46dd3c48c2c99776798346c4c2ff247da388
Student <jakub.sulisz@dci-student.org>
2025-07-04 13:37:13 +0200

    done2

--- backend/data/utilities/connectDB.js
+++ backend/data/utilities/connectDB.js
@@ -5,7 +5,14 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI || DB_URI);
+    if (!process.env.DB_URI) {
+      throw new Error("Missing DB_URI in environment variables");
+    }
+
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected");
   } catch (error) {
     console.error(error.message);
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ import cors from "cors";
 import fs from "fs/promises";
 import path from "path";
 import dotenv from "dotenv";
+dotenv.config();
 import axios from "axios";
 
 import loginRouter from "./routes/login.js";
 import resultsRouter from "./routes/results.js";
 import { connectDB } from "./data/utilities/connectDB.js";
-dotenv.config();
 connectDB();
 
 const resultsPath = path.join(path.resolve(), "data", "lastResults.json");
